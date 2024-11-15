9a92795f4e5bd66dfe8eab7b322dc5231857156c
SHM_white <1131287310a@gmail.com>
2024-11-15 23:45:22 +0800

    1

--- ClassHomework/9/id4.c
+++ ClassHomework/9/id4.c
@@ -12,8 +12,20 @@
 #define max(a, b) ((a) > (b) ? (a) : (b))
 #define min(a, b) ((a) < (b) ? (a) : (b))
 
+void f(int *result, int n){
+    for (size_t i = 1; i <= n; i++)
+    {
+        if(i == 1){
+            *result = 1;
+        }
+        *result *= i;
+    }
+} 
 int main()
 {
-    int m, n;
-
+    int n;
+    int result = 1;
+    scanf("%d", &n);
+    f(&result, n);
+    printf("%d", result);
 }
