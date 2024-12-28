6e2bc49194c7d87bddabe5d61bd4e904c479e06c
SHM-white <1131287310a@gmail.com>
2024-12-28 09:45:38 +0800

    1

--- Comprehensive_testing/funtion/4.c
+++ Comprehensive_testing/funtion/4.c
@@ -11,7 +11,8 @@
 
 #define max(a, b) ((a) > (b) ? (a) : (b))
 #define min(a, b) ((a) < (b) ? (a) : (b))
-int length(char *str){
+int length(char *str)
+{
     int count = 0;
     while (str[count] != '\0')
     {
@@ -19,24 +20,28 @@ int length(char *str){
     }
     return count;
 }
-bool CheckWaitItem(char *origin, int checkPoint, char *target){
+bool CheckWaitItem(char *origin, int checkPoint, char *target)
+{
     bool result = true;
-    for (size_t i = 0;target[i]!='\0'; i++)
+    for (size_t i = 0; target[i] != '\0'; i++)
     {
         result &= (origin[checkPoint + i] == target[i]);
-        if(!result){
+        if (!result)
+        {
             return false;
         }
     }
     return result;
 }
-int countSubStr(char *str, char *target){
+int countSubStr(char *str, char *target)
+{
     int strLen = length(str);
     int waitCount = 0;
     int *waitList = malloc(sizeof(int));
     for (size_t i = 0; i < strLen; i++)
     {
-        if(str[i] == target[0]){
+        if (str[i] == target[0])
+        {
             waitCount++;
             waitList = realloc(waitList, waitCount * sizeof(int));
             waitList[waitCount - 1] = i;
@@ -45,35 +50,54 @@ int countSubStr(char *str, char *target){
     int count = 0;
     for (size_t i = 0; i < waitCount; i++)
     {
-        if(CheckWaitItem(str, waitList[i], target)){
+        if (CheckWaitItem(str, waitList[i], target))
+        {
             count++;
         }
     }
+    free(waitList);
     return count;
 }
-//Sub str in [begin, end]
-void getSubStr( char *source, 
-                char *output, 
-                int begin, 
-                int end)
+// Sub str in [begin, end]
+void getSubStr(char *source,
+               char *output,
+               int begin,
+               int end)
 {
     int i;
-    for (i = begin; i <= end; i++)
+    for (i = begin; i < end; i++)
     {
         output[i - begin] = source[i];
     }
-    output[i] = '\0';
+    output[i - begin] = '\0';
 }
-int main(){
+int main()
+{
     int minLength;
     scanf("%d", &minLength);
     char text[1024];
     scanf("%s", text);
 
+    char findedStr[100];
+    int maxcount = 0;
     int strLen = length(text);
-    for (size_t i = minLength; i < strLen; i++)
+    for (size_t start = 0; start < strLen; start++)
     {
-        
+        for (size_t i = minLength; i < strLen - start + 1; i++)
+        {
+            char subStr[100];
+            getSubStr(text, subStr, start, start + i);
+            int currentCount = countSubStr(text, subStr);
+            if (currentCount > maxcount)
+            {
+                maxcount = currentCount;
+                strcpy(findedStr, subStr);
+            }
+            else if (currentCount == maxcount && length(subStr) > length(findedStr))
+            {
+                strcpy(findedStr, subStr);
+            }
+        }
     }
-    
+    printf("%s", findedStr);
 }
